cdcaaf2f4051bcf3d88510021c3e2d8fc9d9babe
Elvin <Elvin@ElvinGarcia.com>
2022-03-28 21:53:31 -0400

    got latest movie presentational component working.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -1,9 +1,36 @@
 import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
-
-const NYT_API_KEY = 'your-key-here';
+// const URL1 ='https://learn-co-curriculum.github.io/books-json-example-api/books.json'
+const NYT_API_KEY = 'sXzTtSdQeXAmpkX6R2gXyOBk5e38pl8R';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+class LatestMovieReviewsContainer extends Component{
+  constructor(props) {
+    super(props)
+    this.state = {
+      reviews: [],
+    }
+  }
+
+  componentDidMount() {
+    fetch(URL).then((response) => response.json())
+      .then((data) => this.setState({ reviews: data.results }));
+  }
+
+
+  render() {
+    return (
+      this.state.reviews.map((review) => < MovieReviews display_title={review.display_title} summary_short={review.summary_short} src={review.multimedia.src} />)
+    )
+
+
+    }
+
+
+}
+
+
+export default LatestMovieReviewsContainer;
--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,12 @@
 // Code MovieReviews Here
+import React from "react";
+
+const MovieReviews = ({display_title , summary_short , src }) => (
+
+  <ul>
+    <li className="review"><img src={src} /><h3>{display_title}</h3> <p>{summary_short}</p></li>
+  </ul>
+)
+
+
+export default MovieReviews;
