7ebecb16e8b4b8f58dba742d6d8d23e6e7722dc7
Elvin <Elvin@ElvinGarcia.com>
2022-03-30 08:50:26 -0400

    stuck on search functionality

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
+const URL = 'https://learn-co-curriculum.github.io/books-json-example-api/books.json'
 const NYT_API_KEY = 'sXzTtSdQeXAmpkX6R2gXyOBk5e38pl8R';
-const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
-            + `api-key=${NYT_API_KEY}`;
+// const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
+//             + `api-key=${NYT_API_KEY}`;
 
-// Code LatestMovieReviewsContainer Here
+
 class LatestMovieReviewsContainer extends Component{
   constructor(props) {
     super(props)
@@ -22,9 +22,9 @@ class LatestMovieReviewsContainer extends Component{
 
 
   render() {
-    return (
-      this.state.reviews.map((review, index) => < MovieReviews display_title={review.display_title} summary_short={review.summary_short} src={review.multimedia.src} link_url={review.link.url} index={index} />)
-    )
+     return ( ""
+    //   this.state.reviews.map((review, index) => < MovieReviews display_title={review.display_title} summary_short={review.summary_short} src={review.multimedia.src} link_url={review.link.url} index={index} />)
+     )
 
 
     }
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -1,44 +1,50 @@
 import React, { Component } from 'react';
-import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
-
+import 'isomorphic-fetch';
 const NYT_API_KEY = 'sXzTtSdQeXAmpkX6R2gXyOBk5e38pl8R';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
             + `api-key=${NYT_API_KEY}`;
 
-// Code SearchableMovieReviewsContainer Here
+
 class SearchableMovieReviewsContainer extends Component{
   constructor(props) {
     super(props)
     this.state = {
       input: "",
-      result: [],
+      reviews: [],
     };
+  };
 
-
-  }
-
-  submitedForm(event) {
+  handleSubmit(event) {
     event.preventDefault();
-    submitedInput = event.target.query.value
+    fetch(URL + '&query=' + this.state.input)
+      .then((response) => response.json())
+      .then((data) => this.setState({ reviews: data.results }))
+      .catch(console.error('an error occured'))
 
-  //  left here. 'submitedInput' is the value of the form when submmited
   }
 
-  onChange(event) {
-    console.log(event.target.value)
-   const input = event.target.value;
-    this.setState({data:input})
+  handleChange(event) {
+    this.setState({data: `${event.target.value}`})
   }
 
   render() {
     return (
       <div>
-        <form name="form" onSubmit={(event)=>this.submitedForm(event)}>
-          <input type="text" name="query" placeholder='search' onChange={(event) => this.onChange(event)} />
-          <button name="search" type="submit">search</button>
+        <form onSubmit={(event)=>this.handleSubmit(event)}>
+  <input type="text" value={this.state.value} placeholder='search' onChange={(event) => this.handleChange(event)} />
+          <button value="search" type="submit">search</button>
         </form>
+
+        {/* should display the components below if this.state.reviews is not empty. I should be passing the entire state over to < MovieReviews /> component */}
+         < MovieReviews reviews={this.state.reviews} />)
+
+
+
+
       </div>
+
+
     )
   }
 
