9b435b2b5d25986b98ac89725f99b7994fdfcd59
Elvin <Elvin@ElvinGarcia.com>
2022-03-30 22:00:30 -0400

    Restructured the way MovieReview components restructured the way MovieReview components to be more adaptable to other components that pass data to it. Also made the components less error prone by including defaultprops instance methods

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,16 +1,23 @@
 // Code MovieReviews Here
 import React from "react";
 
-const MovieReviews = ({display_title , summary_short , src, link_url, index }) => (
-
-  <ul>
-    <li className="review" key={index}>
-      <a href={link_url}> <img src={src} alt={display_title} /> </a>
-      <h3>{display_title}</h3>
-      <p>{summary_short}</p>
+const Review = ({display_title, link, summary_short, headline, multimedia }) => (
+  <ul key={headline}>
+    <li key={display_title}>
+      <h1>{display_title}</h1>
+      <h2>{headline}</h2>
+      <a href={link.url}><img src={multimedia.src} alt={display_title} /></a>
+      <h3>{summary_short}</h3>
     </li>
   </ul>
-)
+);
 
+
+
+const MovieReviews = ({ reviews }) => reviews.map((Review));
+
+MovieReviews.defaultprops = {
+  reviews: [],
+}
 
 export default MovieReviews;
